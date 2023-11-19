efa60231f86f7792248dc2cdadcdef5fc3e9841c
Jose Garcia <garciacolin@hotmail.com>
2023-11-19 00:28:08 -0600

    Create Form & Control elements

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,8 +1,53 @@
-const Form = () => {
+import { useState } from "react";
+
+const Form = ({newItem}) => {
+
+  const [num, setNum] = useState(0);
+  const [item, setItem] = useState("");
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    // Validate
+    if (item === "" || num === 0) {
+      console.log("All fields are mandatory");
+      return
+    }
+
+    // Create new item
+    const newItem = {
+      num,
+      item,
+      packed: false,
+      id: Date.now()
+    }
+    
+    // Reset form
+    setItem("");
+    setNum(0);
+  }
+
   return (
-    <div className="add-form">
+    <form onSubmit={handleSubmit} className="add-form">
       <h3>What do you need for your trip?</h3>
-    </div>
+      <select value={num} onChange={e => setNum(+e.target.value)}>
+        <option value="">Select</option>
+        {Array.from({length: 20}, (_, i) => i + 1).map(numb => (
+          <option 
+          key={numb}
+          >
+            {numb}
+          </option>
+        ))}
+      </select>
+      <input 
+      type="text" 
+      placeholder="item..."
+      value={item}
+      onChange={e => setItem(e.target.value)}
+      />
+      <button>Add</button>
+    </form>
   );
 };
 
