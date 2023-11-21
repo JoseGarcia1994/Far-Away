f6f7b11bbd6016af2ef7c6d2bdaf0f1d58545631
Jose Garcia <garciacolin@hotmail.com>
2023-11-21 14:15:17 -0600

    Sorting items on the packing list

--- src/components/PackingList.jsx
+++ src/components/PackingList.jsx
@@ -1,11 +1,20 @@
 import Item from './Item.jsx';
+import { useState } from 'react';
 
 const PackingList = ({items, deleteItem, updateItem}) => {
 
+  const [sortBy, setSortBy] = useState("input");
+
+  let sortedItems;
+  
+  if (sortBy === "input") sortedItems = items;
+  if (sortBy === 'description') sortedItems = items.slice().sort((a,b) => a.item.localeCompare(b.item));
+  if (sortBy === 'packed') sortedItems = items.slice().sort((a,b) => Number(a.packed) - Number(b.packed ));
+  
   return (
     <div className="list">
       <ul>
-        {items.map(item => (
+        {sortedItems?.map(item => (
           <Item 
           key={item.id}
           item={item}
@@ -14,6 +23,14 @@ const PackingList = ({items, deleteItem, updateItem}) => {
           />
         ))}
       </ul>
+
+      <div className='actions'>
+          <select value={sortBy} onChange={e => setSortBy(e.target.value)}>
+            <option value='input'>Sort by input order</option>
+            <option value='description'>Sort by description</option>
+            <option value='packed'>Sort by packed status</option>
+          </select>
+      </div>
     </div>
   )
 };
