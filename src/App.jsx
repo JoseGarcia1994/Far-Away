935ca19b9fefb6bfa2cf0c20b9035aa09bf9d0d2
Jose Garcia <garciacolin@hotmail.com>
2023-11-21 12:02:27 -0600

    Calculate Statistics on Status component

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,7 @@ const App = () => {
       <Logo />
       <Form  addItems={addItems}/>
       <PackingList items={items} deleteItem={deleteItem} updateItem={updateItem}/>
-      <Status />
+      <Status items={items}/>
     </div>
   );
 };
--- src/components/Status.jsx
+++ src/components/Status.jsx
@@ -1,7 +1,17 @@
-const Status = () => {
+
+const Status = ({items}) => {
+
+  const totalItems = items.length
+  const numPacked = items.filter(item => item.packed).length
+  const percentage = Math.round((numPacked / totalItems) * 100)
+
   return (
     <footer className="stats">
-      <em>You have X items on your list, and you already packed X</em>
+      {totalItems === 0 ? 
+      <em>Start adding some items to your packing list</em> :
+      <em>
+        {percentage === 100 ? 'You got everthing! Ready to go ✈' : `You have ${totalItems} ${totalItems > 1 ? 'items' : 'item'} on your list, and you already packed ${numPacked} (${totalItems === 0 ? 0 : percentage}%)`}
+      </em>}
     </footer>
   );
 };
